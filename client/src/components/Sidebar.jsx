52bdabf089ebe824b54117104aa81c7c145291cc
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-07-29 22:06:49 +0530

    sidebar logo and menu icon

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import assets from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
-const Sidebar = () => {
+const Sidebar = ({ selectedUser, setSelectedUser }) => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>Sidebar</h1>
+    <div
+      className={`bg-[#8185b2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${
+        selectedUser ? "max-md:hidden" : ""
+      }`}
+    >
+      <div className="pb-5">
+        <div className="flex justify-between items-center">
+          <img src={assets.logo} alt="logo" className="max-w-40" />
+          <div className="relative py-2 group">
+            <img
+              src={assets.menu_icon}
+              alt="menu_icon"
+              className="max-h-5 cursor-pointer"
+            />
+            <div className="absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282142] border border-gray-600 hidden group-hover:block">
+              <p
+                onClick={() => navigate("/profile")}
+                className="cursor-pointer text-sm"
+              >
+                Edit Profile
+              </p>
+              <hr className="border-gray-500 my-2 border-t" />
+              <p className="cursor-pointer text-sm">Logout</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
