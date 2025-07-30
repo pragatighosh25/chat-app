d1425958cb4c96e4951542b25d4678657981f0a2
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-07-30 12:57:24 +0530

    backend setup

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import "dotenv/config";
+import cors from 'cors';
+import http from 'http';
+
+//create express app nd http server
+const app = express();
+const server = http.createServer(app);
+
+//middleware
+app.use(express.json({limit: '5mb'}));
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}));
+
+//routes
+app.get('/api/status', (req, res) => {
+    res.send('Server is running');
+});
+
+//port
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT} http://localhost:${PORT}`);
+});
