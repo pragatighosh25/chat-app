58be8383647b607bbe66d49b52be8861af5831c7
Pragati Ghosh <pragati25ghosh@gmail.com>
2025-08-03 23:54:32 +0530

    update user

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -1,14 +1,14 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.models.js";
 import bcrypt from "bcryptjs";
-
+import cloudinary from "../lib/cloudinary.js";
 //signup a new user
 export const signup = async (req, res) => {
   const { email, fullName, password, bio } = req.body;
 
   try {
     // Validate input
-    if(!fullName || !email || !password) {
+    if (!fullName || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
     }
     // Check if user already exists
@@ -26,17 +26,22 @@ export const signup = async (req, res) => {
       email,
       fullName,
       password: hashedPassword,
-      bio
+      bio,
     });
     const token = generateToken(newUser._id);
 
     await newUser.save();
-    return res.json({ success: true, token, userData: newUser, message: "User created successfully" });
+    return res.json({
+      success: true,
+      token,
+      userData: newUser,
+      message: "User created successfully",
+    });
   } catch (error) {
     console.error("Error during signup:", error.message);
     return res.json({ success: false, message: "Internal server error" });
   }
-}
+};
 
 //login a user
 export const login = async (req, res) => {
@@ -49,30 +54,75 @@ export const login = async (req, res) => {
       return res.json({ success: false, message: "Invalid email or password" });
     }
 
-  // Check if password is correct
-  const isPasswordCorrect = await bcrypt.compare(password, userData.password);
-  if (!isPasswordCorrect) {
-    return res.json({ success: false, message: "Invalid email or password" });
-  }
-  // Generate token and respond
-  const token = generateToken(userData._id);
+    // Check if password is correct
+    const isPasswordCorrect = await bcrypt.compare(password, userData.password);
+    if (!isPasswordCorrect) {
+      return res.json({ success: false, message: "Invalid email or password" });
+    }
+    // Generate token and respond
+    const token = generateToken(userData._id);
 
-  //return user data and token
-  return res.json({ success: true, token, userData, message: "Login successful" });
+    //return user data and token
+    return res.json({
+      success: true,
+      token,
+      userData,
+      message: "Login successful",
+    });
   } catch (error) {
-  console.error("Error during login:", error.message);
-  return res.json({ success: false, message: "Internal server error" });
- }
-}
+    console.error("Error during login:", error.message);
+    return res.json({ success: false, message: "Internal server error" });
+  }
+};
 
 //controller  to check if user is authenticated
 export const isAuthenticated = async (req, res) => {
-    const user = req.user; // user is set by authMiddleware
-    return res.json({ success: true, user });
-}
+  const user = req.user; // user is set by authMiddleware
+  return res.json({ success: true, user });
+};
 
-//controller to get user profile
-export const getUserProfile = async (req, res) => {
-    const user = req.user; // user is set by authMiddleware
-    return res.json({ success: true, user });
+//controller to update user profile
+export const updateProfile = async (req, res) => {
+  try {
+    const { fullName, bio, profilePicture } = req.body;
+    const userId = req.user._id; // user is set by authMiddleware
+
+      // Validate input
+      if (!fullName || !bio) {
+        return res
+          .status(400)
+          .json({ message: "Full name and bio are required" });
+      }
+
+
+    let updatedUser;
+
+    if(!profilePicture) {
+      // Update user without profile picture
+      updatedUser = await User.findByIdAndUpdate(
+        userId,
+        { fullName, bio },
+        { new: true }
+      );
+    }else{
+      // Update user with profile picture
+      const upload= await cloudinary.uploader.upload(profilePicture);
+
+      updatedUser = await User.findByIdAndUpdate(
+        userId,
+        { fullName, bio, profilePicture: upload.secure_url },
+        { new: true }
+      );
+    }
+
+    res.json({
+      success: true,
+      userData: updatedUser,
+      message: "Profile updated successfully",
+    });
+
+   } catch (error) {
+      console.error("Validation error:", error.message);
+      return res.json({ message: "Invalid input", success: false });
+    }
 }
